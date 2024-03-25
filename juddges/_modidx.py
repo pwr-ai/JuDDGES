d7054908b9c4bafee06043741a745cd69d0a9cd7
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-03-25 12:05:37 +0000

    Add script for chunked embeddings

--- juddges/_modidx.py
+++ juddges/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://laugustyniak.github.io',
                 'git_url': 'https://github.com/laugustyniak/juddges',
                 'lib_path': 'juddges'},
-  'syms': {'juddges.data.base': {}, 'juddges.data.pl_court_api': {}, 'juddges.data.pl_court_parser': {}}}
+  'syms': {'juddges.data.pl_court_api': {}, 'juddges.preprocessing.parser_base': {}, 'juddges.preprocessing.pl_court_parser': {}}}
