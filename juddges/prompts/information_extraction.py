53433404cca25c8664f1f5596b5214ab376c0dfa
Łukasz Augustyniak <aisolutions@lukaszaugustyniak.com>
2024-03-27 09:22:09 +0000

    first information extraction schema

---
+++ juddges/prompts/information_extraction.py
@@ -0,0 +1,76 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers.json import parse_json_markdown
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+
+LANGUAGE = "Polish"
+
+PROMPT_TEMPLATE = """Act as a legal document tool that extracts information and answer questions based on judgements. 
+
+Instruction for extracting information from judgements:
+- Judgements are in {LANGUAGE} language, please extract information in {LANGUAGE}.
+- Do not provide information that are not explicitly mentioned in judgements. If you can't extract information from the text field, leave the field with empty string "".
+
+Follow the following YAML structure to extract information and answer questions based on judgements: 
+{SCHEMA}
+
+====
+{TEXT}
+====
+
+Format response as JSON:
+"""
+
+SCHEMA = """
+defendant_gender: string
+defendant_age: integer
+defendant_relationship_status: string
+defendant_has_children: boolean
+defendant_homeless: boolean
+appellant: string
+appeal_against: string
+defendant_plead_or_convicted: string "guilty plea" or "convicted at trial"
+jury_unanimous: string "unanimous" or "other"
+first_trial: boolean
+drug_offence: boolean
+original_sentence: string
+tried_court_type: string "Crown" or "magistrates'"
+single_transaction_multiple_offence: boolean
+multiple_transactions_period: string "within 1 year" or "more than 1 year"
+concurrent_or_consecutive_sentence: string "concurrently" or "consecutively"
+sentence_on_top_existing: boolean
+sentence_adding_up: boolean
+sentence_leniency: string "unduly lenient" or "too excessive"
+guilty_plea_reduction_reason: string
+sentence_discount_mention: boolean
+totality_issues_similar_offences: boolean
+sentence_proportionality_mention: boolean
+sentence_type_issues_totality: boolean
+sentence_adjustment_mention: boolean
+offender_culpability_determination: boolean
+harm_caused_determination: boolean
+offence_seriousness: boolean
+aggravating_factors: string list of factors excluding previous convictions
+previous_convictions_similarity: string "similar" or "dissimilar"
+mitigating_factors: string list of factors
+immediate_sentence_concurrency: boolean
+totality_conflicting_guidelines: boolean
+totality_principle_misapplication: boolean
+"""
+
+
+def prepare_information_extraction_chain(
+    model_name: str = "gpt-4-0125-preview", log_to_mlflow: bool = False
+):
+    model = ChatOpenAI(model=model_name, temperature=0)
+    human_message_template = HumanMessagePromptTemplate.from_template(PROMPT_TEMPLATE)
+    _prompt = ChatPromptTemplate(
+        messages=[human_message_template],
+        input_variables=["TEXT", "LANGUAGE", "SCHEMA"],
+    )
+
+    if log_to_mlflow:
+        import mlflow
+
+        mlflow.log_dict(_prompt.save_to_json(), "prompt.json")
+
+    return _prompt | model | (lambda x: parse_json_markdown(x.content))
---
+++ juddges/settings.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+import tiktoken
+
+# get root path as ROOT_PATH as pathlib objects
+ROOT_PATH = Path(__file__).resolve().parent.parent
+
+DATA_PATH = ROOT_PATH / "data"
+
+MLFLOW_EXP_NAME = "Juddges-Information-Extraction"
+
+
+def num_tokens_from_string(
+    string: str,  # The string to count tokens for
+    encoding_name: str = "cl100k_base",  # gpt-4, gpt-3.5-turbo, text-embedding-ada-002
+) -> int:  # The number of tokens in the string
+    """
+    Returns the number of tokens in a text string.
+    """
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+LLM_TO_PRICE_INPUT = {
+    "gpt-4-1106-preview": 0.01 / 1000,
+    "gpt-4-0125-preview": 0.01 / 1000,
+    "gpt-3.5-turbo-1106": 0.001 / 1000,
+}
+
+LLM_TO_PRICE_COMPLETION = {
+    "gpt-4-1106-preview": 0.03 / 1000,
+    "gpt-4-0125-preview": 0.03 / 1000,
+    "gpt-3.5-turbo-1106": 0.002 / 1000,
+}
+
+LOCAL_POSTGRES = "postgresql+psycopg2://llm:llm@postgres-juddges:5432/llm"
+
+
+def get_sqlalchemy_engine():
+    from sqlalchemy import create_engine
+
+    return create_engine(
+        LOCAL_POSTGRES,
+        pool_size=10,
+        max_overflow=2,
+        pool_recycle=300,
+        pool_pre_ping=True,
+        pool_use_lifo=True,
+    )
+
+
+def prepare_langchain_cache():
+    import langchain
+    from langchain.cache import SQLAlchemyMd5Cache
+
+    langchain.llm_cache = SQLAlchemyMd5Cache(get_sqlalchemy_engine())
+
+
+def prepare_mlflow(experiment_name: str = MLFLOW_EXP_NAME, url="http://host.docker.internal"):
+    import mlflow
+
+    mlflow.set_tracking_uri()
+    mlflow.set_experiment(experiment_name)
