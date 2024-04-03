249385e70a1d5c28424e8dfe17373cd4ffc72847
Jakub Binkowski <jakub.binkowski@pwr.edu.pl>
2024-04-03 12:45:00 +0200

    Parse pl judgements (#4)
    * Improve error handling in download_pl_content.py
    
    * Add dataset dump scrip
    
    * Add pl dataset to DVC
    
    * Add simple data analysis notebook
    
    * Extract text from pl judgements
    
    * Refine text extraction and add analysis
    
    * Add addtional details download and ingest
    
    * Refine extraction and ingest extracted data to mongo
    
    * Add script for chunked embeddings
    
    ---------
    
    Co-authored-by: Jakub Binkowski <binkjakub@gmail.com>

--- juddges/data/pl_court_api.py
+++ juddges/data/pl_court_api.py
@@ -3,11 +3,40 @@ from typing import Any
 import requests
 import xmltodict
 from loguru import logger
+from requests import HTTPError
 
 
 class PolishCourtAPI:
     def __init__(self) -> None:
         self.url = "https://apiorzeczenia.wroclaw.sa.gov.pl/ncourt-api"
+
+    @property
+    def schema(self) -> dict[str, list[str]]:
+        return {
+            "judgement": [
+                "_id",
+                "signature",
+                "date",
+                "publicationDate",
+                "lastUpdate",
+                "courtId",
+                "departmentId",
+                "type",
+                "excerpt",
+            ],
+            "content": ["content"],
+            "details": [
+                "chairman",
+                "judges",
+                "themePhrases",
+                "references",
+                "legalBases",
+                "recorder",
+                "decision",
+                "reviser",
+                "publisher",
+            ],
+        }
 
     def get_number_of_judgements(self, params: dict[str, Any] | None = None) -> int:
         if params is None:
@@ -33,11 +62,45 @@ class PolishCourtAPI:
 
         return judgements
 
-    def get_content(self, id: str) -> str:
+    def get_content(self, id: str) -> dict[str, Any]:
         params = {"id": id}
         endpoint = f"{self.url}/judgement/content"
         res = requests.get(endpoint, params=params)
-        res.raise_for_status()
+
+        try:
+            res.raise_for_status()
+        except HTTPError as err:
+            if err.response.status_code == 404:
+                raise DataNotFoundError(f"Not found content for document: {id}")
+            raise
+
         content = res.content.decode("utf-8")
 
-        return content
+        return {"content": content}
+
+    def get_cleaned_details(self, id: str) -> dict[str, Any]:
+        """Downloads details without repeating fields retrieved in get_judgements."""
+        details = self.get_details(id)
+        return {k: v for k, v in details.items() if k in self.schema["details"]}
+
+    def get_details(self, id: str) -> dict[str, Any]:
+        params = {"id": id}
+        endpoint = f"{self.url}/judgement/details"
+        res = requests.get(endpoint, params=params)
+        res.raise_for_status()
+
+        # for details, API returns XML with error info instead of 404 status code
+        data = xmltodict.parse(res.content.decode("utf-8"))
+        try:
+            details = data["judgement"]
+        except KeyError:
+            if "error" in data.keys():
+                raise DataNotFoundError(f"Not found details for document: {id}")
+            raise
+        else:
+            assert isinstance(details, dict)
+            return details
+
+
+class DataNotFoundError(Exception):
+    pass
