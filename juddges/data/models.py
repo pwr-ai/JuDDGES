80893fa977b056d64fb6c1335e18a30041f9bf07
Łukasz Augustyniak <aisolutions@lukaszaugustyniak.com>
2024-04-09 10:37:12 +0200

    Instruction dataset v1 (#5)
    * first information extraction schema
    
    * mlflow tracking
    
    * add streamlit app
    
    * v1 prompt ready
    
    * nbdev
    
    * get mongo docs
    
    * Parse pl judgements (#4)
    
    * Improve error handling in download_pl_content.py
    
    * Add dataset dump scrip
    
    * Add pl dataset to DVC
    
    * Add simple data analysis notebook
    
    * Extract text from pl judgements
    
    * Refine text extraction and add analysis
    
    * Add addtional details download and ingest
    
    * Refine extraction and ingest extracted data to mongo
    
    * Add script for chunked embeddings
    
    ---------
    
    Co-authored-by: Jakub Binkowski <binkjakub@gmail.com>
    
    * first information extraction schema
    
    * mlflow tracking
    
    * add streamlit app
    
    * v1 prompt ready
    
    * nbdev
    
    * get mongo docs
    
    * update nbdev
    
    * small fixes
    
    ---------
    
    Co-authored-by: Jakub Binkowski <jakub.binkowski@pwr.edu.pl>
    Co-authored-by: Jakub Binkowski <binkjakub@gmail.com>

---
+++ juddges/data/models.py
@@ -0,0 +1,17 @@
+import os
+
+from pymongo import MongoClient
+from pymongo.collection import Collection
+
+if os.environ.get("MONGO_URI", None) is None:
+    raise Exception("Missing `MONGO_URI` environment variable.")
+
+
+if os.environ.get("MONGO_DB_NAME", None) is None:
+    raise Exception("Missing `MONGO_DB_NAME` environment variable.")
+
+
+def get_mongo_collection(collection_name: str) -> Collection:
+    client = MongoClient(os.environ["MONGO_URI"])
+    db = client[os.environ["MONGO_DB_NAME"]]
+    return db[collection_name]
