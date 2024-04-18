d62c7c5e4e86ccdd29e1436262760573edb21dcc
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-04-18 14:03:54 +0000

    Refine instruction dataset

--- scripts/sft/build_fine_tuning_dataset.py
+++ scripts/sft/build_fine_tuning_dataset.py
@@ -13,37 +13,48 @@ from juddges.settings import PL_JUDGEMENTS_PATH_INSTRUCT, PL_JUDGEMENTS_PATH_RAW
 
 load_dotenv()
 
+MAX_SHARD_SIZE = "4GB"
+
 SCHEMA_TEMPLATE = "```yaml\n{schema}\n```"
 INSTRUCTION_TEMPLATE = """
 You are extracting information from the court judgment.
-Extract specified values strictly from the provided judgement. Return answer in YAML format:
+Extract specified values strictly from the provided judgement.
+If information is not provided in the judgement, leave the field with null value.
+Return answer in YAML format, without any additional information outside YAML:
 {schema}
 =====
 {{context}}
 ======
 """
+SCHEMA_DESC = {
+    "date": "date in format YYYY-MM-DD",
+    "judges": "list of judge full names",
+    "recorder": "string containing the name of the recorder",
+    "signature": "string contraining the signature of the judgment",
+}
+PROMPT = INSTRUCTION_TEMPLATE.format(
+    schema=SCHEMA_TEMPLATE.format(schema=yaml.dump(SCHEMA_DESC).strip())
+)
+
+FEATURES = ["date", "judges", "recorder", "signature", "text"]
 
 
 def main(
     dataset_dir: Path = typer.Option(PL_JUDGEMENTS_PATH_RAW, help="Path to the dataset directory"),
     repo_id: Optional[str] = typer.Option(None),
     target_dir: Path = typer.Option(
-        PL_JUDGEMENTS_PATH_INSTRUCT, help="Path to the target directory"
+        PL_JUDGEMENTS_PATH_INSTRUCT,
+        help="Path to the target directory",
     ),
     test_size: int = typer.Option(2_000, help="Size of the test set"),
     random_seed: int = typer.Option(42, help="Random seed"),
     num_jobs: Optional[int] = typer.Option(
-        None, envvar="NUM_JOBS", help="Number of parallel jobs to use"
+        None,
+        envvar="NUM_JOBS",
+        help="Number of parallel jobs to use",
     ),
 ):
-    feature_cols = [
-        "_id",
-        "signature",
-        "date",
-        "chairman",
-        "judges",
-        "text",
-    ]
+    feature_cols = ["_id"] + FEATURES
     ds = load_dataset("parquet", name="pl_judgements", data_dir=dataset_dir)
     logger.info("Loading dataset...")
     ds = ds.select_columns(feature_cols)
@@ -54,17 +65,19 @@ def main(
     ds = ds.filter(_filter)
 
     logger.info("Generating instructions...")
-    ds = ds.map(to_instruction_fmt, num_proc=num_jobs, remove_columns=feature_cols)
+    ds = ds.map(to_instruction_fmt, num_proc=num_jobs, remove_columns=FEATURES)
     ds = ds["train"].train_test_split(test_size=test_size, seed=random_seed)
 
+    logger.info("Built dataset with following parameters: {ds_info}", ds_info=str(ds))
+
     if repo_id:
-        ds.push_to_hub(repo_id, max_shard_size="4GB")
+        ds.push_to_hub(repo_id, max_shard_size=MAX_SHARD_SIZE)
     else:
-        ds.save_to_disk(target_dir, max_shard_size="4GB", num_proc=num_jobs)
+        ds.save_to_disk(target_dir, max_shard_size=MAX_SHARD_SIZE, num_proc=num_jobs)
 
 
 def _pre_filter(item: dict[str, Any]) -> bool:
-    return item["date"] is not None and item["judges"] is not None and item["text"] is not None
+    return all(item[feat] is not None for feat in FEATURES)
 
 
 def _preprocess(item: dict[str, Any]) -> dict[str, Any]:
@@ -92,29 +105,18 @@ def _split_multiple_names(item: dict[str, Any]) -> dict[str, Any]:
 
 def _filter(item: dict[str, Any]) -> bool:
     all_judges_in_text = all(j in item["text"] for j in item["judges"])
+    recorder_in_text = item["recorder"] in item["text"]
     signature_in_text = item["signature"] in item["text"]
 
-    return all_judges_in_text and signature_in_text
+    return all_judges_in_text and recorder_in_text and signature_in_text
 
 
 def to_instruction_fmt(item: dict[str, Any]) -> str:
-    schema_desc = {
-        "date": "date in format YYYY-MM-DD",
-        "judges": "list of judge full names",
-        "signature": "string contraining the signature of the judgment",
-    }
-
     output = SCHEMA_TEMPLATE.format(
-        schema=yaml.dump({k: item[k] for k in schema_desc.keys()}).strip()
+        schema=yaml.dump({k: item[k] for k in SCHEMA_DESC.keys()}).strip()
     )
 
-    return {
-        "prompt": INSTRUCTION_TEMPLATE.format(
-            schema=SCHEMA_TEMPLATE.format(schema=yaml.dump(schema_desc).strip())
-        ),
-        "context": item["text"],
-        "output": output,
-    }
+    return {"prompt": PROMPT, "context": item["text"], "output": output}
 
 
 if __name__ == "__main__":
