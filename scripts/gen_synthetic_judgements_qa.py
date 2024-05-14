53dc2ec8671d226eb2f59b12ff47496de5305158
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2024-05-14 22:21:49 +0000

    feat: model cfg handling to gen synth qa script

--- scripts/gen_synthetic_judgements_qa.py
+++ scripts/gen_synthetic_judgements_qa.py
@@ -7,6 +7,7 @@ from typing import List, Tuple
 
 import deepl
 import typer
+import yaml
 from joblib import Memory
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_core.prompts import ChatPromptTemplate
@@ -16,16 +17,17 @@ from loguru import logger
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-from juddges.data.models import (
-    LangCode,
-    QAGenerationJudgementMetadata,
-    SyntheticQAPairs,
-)
+from juddges.data.models import LangCode, QAGenerationJudgementMetadata, SyntheticQAPairs
 from juddges.data.qa_pairs_json_parser import QAPairsJsonParser
 from juddges.data.utils import path_safe_udate, read_jsonl, save_jsonl
 from juddges.exception import LanguageMismatchError
 from juddges.prompts.technique import PromptingTechnique
-from juddges.settings import CACHE_DIR, SAMPLE_DATA_PATH, prepare_langchain_cache
+from juddges.settings import (
+    CACHE_DIR,
+    DATA_GEN_MODELS_CONFIGS_DIR,
+    SAMPLE_DATA_PATH,
+    prepare_langchain_cache,
+)
 
 prepare_langchain_cache()
 deepl.http_client.user_agent = os.environ["DEEPL_USER_AGENT"]
@@ -82,6 +84,10 @@ def handle_lang_mismatch(
 
 
 def main(
+    model_cfg: str = typer.Option(
+        default=str(DATA_GEN_MODELS_CONFIGS_DIR / "CapybaraHermes-2.5-Mistral-7B-GPTQ.yaml"),
+        help="Path to the model .yaml configuration file",
+    ),
     judgements_fpath: str = typer.Option(
         default=str(SAMPLE_DATA_PATH / "judgements_sample50_20240427_220002f908780.jsonl"),
         help="Dumped `judgements` collection file path",
@@ -97,35 +103,16 @@ def main(
     ),
     out_smith_dataset: str = typer.Option(default=None, help="LangSmith dataset name"),
     out: str = typer.Option(default=None, help="Output file path"),
-    hf_model: str = typer.Option(
-        help="Hugging Face model name or path",
-        # default="TheBloke/Mistral-7B-Instruct-v0.2-GPTQ",
-        # default="microsoft/Phi-3-mini-128k-instruct",
-        default="TheBloke/CapybaraHermes-2.5-Mistral-7B-GPTQ",
-    ),
-    max_input_length: int = typer.Option(
-        # default=32_000,  # TheBloke/Mistral-7B-Instruct-v0.2-GPTQ
-        # default=128_000,  # microsoft/Phi-3-mini-128k-instruct
-        default=3551,  # TheBloke/CapybaraHermes-2.5-Mistral-7B-GPTQ
-        help="Maximum number of tokens of the prompt and the context",
-    ),
     lang: LangCode = typer.Option(
         default=LangCode.POLISH.value,
         show_choices=True,
         help="Language code (ISO 639-1) of the given data",
     ),
-    max_new_tokens: int = typer.Option(
-        default=512, help="Maximum number of tokens in generated text"
-    ),
-    temperature: float = typer.Option(default=0.7, help="Temperature for text generation"),
-    top_p: float = typer.Option(default=0.95, help="Top-p for text generation"),
-    top_k: int = typer.Option(default=40, help="Top-k for text generation"),
-    repetition_penalty: float = typer.Option(
-        default=1.1, help="Repetition penalty for text generation"
-    ),
 ) -> None:
-    translator = deepl.Translator(os.environ["DEEPL_AUTH_KEY"])
     ts_suffix = path_safe_udate()
+    with open(model_cfg) as f:
+        model_cfg_ = yaml.safe_load(f)
+    translator = deepl.Translator(os.environ["DEEPL_AUTH_KEY"])
     parent_module_path, prompt_template_varname = prompt_template_libpath.rsplit(".", maxsplit=1)
     prompt_src = import_module(parent_module_path)
     prompt_template = getattr(prompt_src, prompt_template_varname)
@@ -149,7 +136,7 @@ def main(
         dataset_name=(out_smith_dataset if out_smith_dataset is not None else dataset_default_name),
         data_type=DataType.kv,
         description=(
-            f"Synthetic QA pairs generated from `{judgements_fpath}` with `{hf_model}`"
+            f"Synthetic QA pairs generated from `{judgements_fpath}` with `{model_cfg_['model']}`"
             f" model and `{prompting_technique.value}` prompting technique"
         ),
     )
@@ -169,23 +156,18 @@ def main(
     logger.info("Loading Hugging Face model and tokenizer...")
 
     model = AutoModelForCausalLM.from_pretrained(
-        hf_model,
+        model_cfg_["model"],
         trust_remote_code=True,
         device_map="auto",
     )
     logger.debug(f"{model.device=}")
 
-    tokenizer = AutoTokenizer.from_pretrained(hf_model, use_fast=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_cfg_["model"], use_fast=True)
     text_gen_pipeline = pipeline(
         task="text-generation",
         model=model,
         tokenizer=tokenizer,
-        max_new_tokens=max_new_tokens,
-        do_sample=True,
-        temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
-        repetition_penalty=repetition_penalty,
+        **model_cfg_["generate_kwargs"],
     )
     hf_pipeline = HuggingFacePipeline(pipeline=text_gen_pipeline)
 
@@ -254,7 +236,7 @@ def main(
         logger.debug(f"Creating example for {judgement['_id']=}...")
         example_metadata = {
             "judgement_id": judgement["_id"],
-            "hf_model": hf_model,
+            "hf_model": model_cfg_["model"],
             "prompt_template": prompt_template,
             "prompting_technique": prompting_technique.value,
             "pairs_count": len(dto),
