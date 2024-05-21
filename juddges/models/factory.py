72dd7eba0ff918b977c73c419caf2134b01f4647
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-05-21 09:05:26 +0000

    Restore tokenizer padding

--- juddges/models/factory.py
+++ juddges/models/factory.py
@@ -23,7 +23,7 @@ def get_model(llm_name: str, **kwargs) -> ModelForGeneration:
 def get_llama_3(llm_name: str, device_map: str) -> ModelForGeneration:
     assert llm_name.startswith("meta-llama")
     model, tokenizer = _get_model_tokenizer(llm_name, device_map)
-    # tokenizer.padding_side = "left"
+    tokenizer.padding_side = "left"
     tokenizer.pad_token = tokenizer.eos_token
     terminators: list[int] = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
 
@@ -37,7 +37,7 @@ def get_llama_3(llm_name: str, device_map: str) -> ModelForGeneration:
 def get_mistral(llm_name: str, device_map: str) -> ModelForGeneration:
     assert "mistral" in llm_name.lower()
     model, tokenizer = _get_model_tokenizer(llm_name, device_map)
-    # tokenizer.padding_side = "left"
+    tokenizer.padding_side = "left"
     tokenizer.pad_token = tokenizer.eos_token
 
     return ModelForGeneration(
