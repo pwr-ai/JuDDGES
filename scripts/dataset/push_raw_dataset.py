8bc75deea655aa754230fb99e99b182a265d92c3
Albert Sawczyn <albert.sawczyn@pwr.edu.pl>
2024-06-04 20:01:43 +0200

    feat: add push instruct dataset script

--- scripts/dataset/push_raw_dataset.py
+++ scripts/dataset/push_raw_dataset.py
@@ -36,8 +36,7 @@ def main(
     card_data = DatasetCardData(
         language="pl",
         multilinguality="monolingual",
-        language_creators=["found"],
-        size_categories="100K<n<1M",
+          size_categories="100K<n<1M",
         source_datasets=["original"],
         pretty_name="Polish Court Judgments Raw",
         tags=["polish court"],
