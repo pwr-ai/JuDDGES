80f700255539f4e623ff5c64f0285e8d13abc335
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-07-03 10:09:40 +0000

    Refactor and make deterministic predictions

--- scripts/sft/predict.py
+++ scripts/sft/predict.py
@@ -1,7 +1,8 @@
 import json
 import os
-import time
 from pathlib import Path
+from pprint import pformat
+from typing import Any
 
 import hydra
 import torch
@@ -9,11 +10,11 @@ from datasets import load_dataset
 from loguru import logger
 from omegaconf import DictConfig
 from openai import BaseModel
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+from pydantic import Field
 
 from juddges.config import DatasetConfig, LLMConfig
 from juddges.models.factory import get_model
+from juddges.models.predict import predict_with_llm
 from juddges.preprocessing.text_encoder import TextEncoderForEval
 from juddges.settings import CONFIG_PATH
 from juddges.utils.config import resolve_config
@@ -31,15 +32,15 @@ class PredictConfig(BaseModel, extra="forbid"):
     device_map: str
     output_file: Path
     metrics_file: Path
-    max_new_tokens: int
     truncate_context: bool
+    generate_kwargs: dict[str, Any] = Field(default_factory=dict)
 
 
 @torch.inference_mode()
 @hydra.main(version_base="1.3", config_path=str(CONFIG_PATH), config_name="predict.yaml")
 def main(cfg: DictConfig) -> None:
     cfg_dict = resolve_config(cfg)
-    logger.info(f"config:\n{cfg_dict}")
+    logger.info(f"config:\n{pformat(cfg_dict)}")
     config = PredictConfig(**cfg_dict)
 
     output_file = Path(config.output_file)
@@ -49,6 +50,10 @@ def main(cfg: DictConfig) -> None:
     logger.info("Loading model...")
 
     model_pack = get_model(config.model, device_map=config.device_map)
+
+    assert not any(key in model_pack.generate_kwargs for key in config.generate_kwargs.keys())
+    model_pack.generate_kwargs |= config.generate_kwargs
+
     model, tokenizer = model_pack.model, model_pack.tokenizer
     model.eval()
     if config.model.batch_size > 1 and config.model.padding is False:
@@ -62,44 +67,20 @@ def main(cfg: DictConfig) -> None:
         padding=config.model.padding,
     )
     ds.set_transform(encoder, columns=["prompt", "context"])
-    dataloader = DataLoader(
-        ds,
+
+    model_outputs = predict_with_llm(
+        model_pack=model_pack,
+        dataset=ds,
         batch_size=config.model.batch_size,
-        num_workers=NUM_PROC,
-        pin_memory=(NUM_PROC > 1),
-        shuffle=False,
+        num_proc=NUM_PROC,
+        verbose=True,
     )
-
-    model_outputs = []
-
-    with tqdm(dataloader) as pbar:
-        for batch in pbar:
-            model_inputs = batch["input_ids"].view(config.model.batch_size, -1)
-            model_inputs = model_inputs.to(DEVICE, non_blocking=True)
-            input_length = model_inputs.size(1)
-
-            start_time = time.time()
-            generated_ids = model.generate(
-                model_inputs,
-                max_new_tokens=config.max_new_tokens,
-                **model_pack.generate_kwargs,
-            )
-            duration = time.time() - start_time
-
-            decoded = tokenizer.batch_decode(
-                generated_ids[:, input_length:],
-                skip_special_tokens=True,
-            )
-            model_outputs.extend(decoded)
-
-            pbar.set_postfix_str(f"{generated_ids.numel() / duration: 0.2f} tok/sec")
-
     results = [
         {"answer": ans, "gold": gold_ans} for ans, gold_ans in zip(model_outputs, gold_outputs)
     ]
 
     with open(output_file, "w") as f:
-        json.dump(results, f, indent="\t")
+        json.dump(results, f, indent="\t", ensure_ascii=False)
 
 
 if __name__ == "__main__":
