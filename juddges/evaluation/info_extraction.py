3a06f31ccebfc2682d8c60a5dd921b8da29aa93c
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-07-08 14:05:34 +0000

    Refactor evaluation code

--- juddges/evaluation/info_extraction.py
+++ juddges/evaluation/info_extraction.py
@@ -1,27 +1,37 @@
-from torchmetrics.functional.text import chrf_score
+import tqdm
 
+from juddges.evaluation.eval_full_text import FullTextCHRFScorer
+from juddges.evaluation.eval_structured import StructuredCHRFEvaluator
 from juddges.evaluation.parse import parse_results
 
 
-def evaluate_extraction(results: list[dict[str, str]]) -> dict[str, float | dict[str, float]]:
-    """Evaluates information extraction by computing metrics per each field."""
-    res_gold, res_pred = parse_results(results)
-    full_text_chrf = full_text_chrf_score(results)
-    per_field_chrf_score = extraction_chrf_score(preds=res_pred, gold=res_gold)
-    return {"full_text_chrf": full_text_chrf, "field_chrf": per_field_chrf_score}
+class InfoExtractionEvaluator:
+    def __init__(self, verbose: bool = True):
+        self.verbose = verbose
 
+        self.structured_evaluators = [
+            StructuredCHRFEvaluator(),
+        ]
+        self.full_text_evaluators = [
+            FullTextCHRFScorer(),
+        ]
 
-def full_text_chrf_score(results: list[dict[str, str]]) -> float:
-    preds, golds = [r["answer"] for r in results], [r["gold"] for r in results]
-    return chrf_score(preds=preds, target=golds, n_word_order=0).item()  # type: ignore
+    def evaluate(self, results: list[dict[str, str]]) -> dict[str, dict[str, float]]:
+        results = {}
+        preds, golds = parse_results(results)
 
+        for eval in (
+            pbar := tqdm(self.structured_evaluators, desc="Structured", disable=not self.verbose)
+        ):
+            pbar.set_postfix({"eval": eval.name})
+            results[eval.name] = eval.evaluate(preds=preds, golds=golds)
 
-def extraction_chrf_score(
-    preds: dict[str, list[str]],
-    gold: dict[str, list[str]],
-) -> dict[str, float]:
-    per_field_chrf_score = {
-        key: chrf_score(preds=preds[key], target=gold[key], n_word_order=0).item()  # type: ignore
-        for key in gold.keys()
-    }
-    return per_field_chrf_score
+        text_preds = [res["answer"] for res in results]
+        text_golds = [res["gold"] for res in results]
+        for eval in (
+            pbar := tqdm(self.full_text_evaluators, desc="Full text", disable=not self.verbose)
+        ):
+            pbar.set_postfix({"eval": eval.name})
+            results[eval.name] = eval.evaluate(preds=text_preds, golds=text_golds)
+
+        return results
