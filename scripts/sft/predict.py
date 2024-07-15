5c23a9136dba12a2bf474ee3c96c2bf2e3f78308
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-07-15 13:23:25 +0000

    Add optimized unsloth inference and inference telemetry

--- scripts/sft/predict.py
+++ scripts/sft/predict.py
@@ -6,7 +6,7 @@ from typing import Any
 
 import hydra
 import torch
-from datasets import Dataset, load_dataset
+from datasets import load_dataset
 from lightning_fabric import seed_everything
 from loguru import logger
 from omegaconf import DictConfig
@@ -19,6 +19,7 @@ from juddges.models.predict import predict_with_llm
 from juddges.preprocessing.text_encoder import TextEncoderForEval
 from juddges.settings import CONFIG_PATH
 from juddges.utils.config import resolve_config
+from juddges.utils.misc import sort_dataset_by_input_length
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 NUM_PROC = int(os.getenv("NUM_PROC", 1))
@@ -37,8 +38,8 @@ class PredictConfig(BaseModel, extra="forbid"):
     random_seed: int
 
 
-@torch.inference_mode()
 @hydra.main(version_base="1.3", config_path=str(CONFIG_PATH), config_name="predict.yaml")
+@torch.inference_mode()
 def main(cfg: DictConfig) -> None:
     cfg_dict = resolve_config(cfg)
     logger.info(f"config:\n{pformat(cfg_dict)}")
@@ -48,7 +49,7 @@ def main(cfg: DictConfig) -> None:
     output_file.parent.mkdir(parents=True, exist_ok=True)
 
     ds = load_dataset(config.dataset.name, split="test")
-    ds, _ = sort_dataset_by_text_length(ds, config.dataset.context_field)
+    ds, reverse_sort_idx = sort_dataset_by_input_length(ds, config.dataset.context_field)
     logger.info("Loading model...")
 
     model_pack = get_model(config.model, device_map=config.device_map)
@@ -56,15 +57,21 @@ def main(cfg: DictConfig) -> None:
     assert not any(key in model_pack.generate_kwargs for key in config.generate_kwargs.keys())
     model_pack.generate_kwargs |= config.generate_kwargs
 
-    model, tokenizer = model_pack.model, model_pack.tokenizer
-    model.eval()
+    if config.model.use_unsloth:
+        from unsloth import FastLanguageModel
+
+        FastLanguageModel.for_inference(model_pack.model)
+    else:
+        model_pack.model.compile()
+        model_pack.model.eval()
+
     if config.model.batch_size > 1 and config.model.padding is False:
         raise ValueError("Padding has to be enabled if batch size > 1.")
 
     gold_outputs = [item["output"] for item in ds]
 
     encoder = TextEncoderForEval(
-        tokenizer=tokenizer,
+        tokenizer=model_pack.tokenizer,
         max_length=config.model.max_seq_length,
         padding=config.model.padding,
     )
@@ -81,18 +88,10 @@ def main(cfg: DictConfig) -> None:
     results = [
         {"answer": ans, "gold": gold_ans} for ans, gold_ans in zip(model_outputs, gold_outputs)
     ]
+    results = [results[i] for i in reverse_sort_idx]
 
     with open(output_file, "w") as f:
         json.dump(results, f, indent="\t", ensure_ascii=False)
-
-
-def sort_dataset_by_text_length(ds: Dataset, field: str) -> tuple[Dataset, torch.Tensor]:
-    lenghts = torch.tensor(
-        ds.map(lambda x: {"length": len(x[field])}, desc="Computing length")["length"]
-    )
-    sort_idx = torch.argsort(lenghts, descending=True, stable=True)
-    ds = ds.select(sort_idx)
-    return ds, sort_idx
 
 
 if __name__ == "__main__":
