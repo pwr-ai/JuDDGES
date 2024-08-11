48a38beebb8a2eb685a884cb22238fe23b7a45a8
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-08-11 07:53:39 +0000

    Add llm-as-judge preliminary results (too much non-evaluable)

--- scripts/sft/evaluate_llm_as_judge.py
+++ scripts/sft/evaluate_llm_as_judge.py
@@ -1,35 +1,36 @@
 import json
+import logging
 import os
 from pathlib import Path
 from pprint import pformat
-from typing import Any, Literal
+from typing import Any
 
 import hydra
-import requests
 import torch
-from deepdiff import DeepDiff
 from dotenv import load_dotenv
 from langsmith.wrappers import wrap_openai
 from loguru import logger
 from omegaconf import DictConfig
 from openai import OpenAI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from juddges.evaluation.eval_structured_llm_judge import StructuredLLMJudgeEvaluator
+from juddges.evaluation.parse import parse_results
 from juddges.settings import CONFIG_PATH
 from juddges.utils.config import resolve_config
 
-LLAMA_CPP_ENDOPOINT = os.getenv("LLAMA_CPP_ENDPOINT", "http://localhost:8000")
+logging.getLogger("openai").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") or "mocked_key"
+OPENAI_ENDPOINT = os.getenv("OPENAI_ENDPOINT")
 
 
 class ApiModel(BaseModel, extra="forbid"):
     name: str
-    api: Literal["llama.cpp", "openai"]
-    endpoint: str = Field(default=LLAMA_CPP_ENDOPOINT)
+    endpoint: str | None = OPENAI_ENDPOINT
     use_langsmith: bool
-    config: dict[str, Any] | None = None
 
 
 class LLMJudgeConfig(BaseModel, extra="forbid"):
@@ -45,22 +46,15 @@ def main(cfg: DictConfig) -> None:
     logger.info(f"config:\n{pformat(cfg_dict)}")
     config = LLMJudgeConfig(**cfg_dict)
 
-    if config.api_model.api == "llama.cpp":
-        results = evaluate_with_llama_cpp(config)
-    elif config.api_model.api == "openai":
-        raise NotImplementedError("OpenAI API is not implemented yet")
-    else:
-        raise ValueError(f"Unknown API: {config.api_model.api}")
+    results = evaluate_with_api_llm(config)
 
-    with open(config.out_metric_file, "w") as f:
+    with config.out_metric_file.open("w") as f:
         json.dump(results, f, indent="\t")
 
 
-def evaluate_with_llama_cpp(config: LLMJudgeConfig) -> dict[str, Any]:
-    _check_llama_cpp_integrity(config)
-
+def evaluate_with_api_llm(config: LLMJudgeConfig) -> dict[str, Any]:
     oai_client = OpenAI(
-        api_key="not-required",
+        api_key=OPENAI_API_KEY,
         base_url=config.api_model.endpoint,
     )
     if config.api_model.use_langsmith:
@@ -70,21 +64,12 @@ def evaluate_with_llama_cpp(config: LLMJudgeConfig) -> dict[str, Any]:
         oai_client=oai_client,
         model_name=config.api_model.name,
     )
+
     with open(config.answers_file) as f:
         answers = json.load(f)
+    preds, golds = parse_results(answers)
 
-    return evaluator.evaluate(answers)
-
-
-def _check_llama_cpp_integrity(config: LLMJudgeConfig) -> None:
-    props = _get_llama_props(config.api_model.endpoint)
-    diff = DeepDiff(config.api_model.config, props, ignore_order=True)
-    if diff:
-        raise ValueError(f"Config mismatch:\n{pformat(diff)}")
-
-
-def _get_llama_props(endpoint: str) -> dict[str, Any]:
-    return requests.get(f"{endpoint}/props").json()
+    return evaluator.evaluate(preds=preds, golds=golds)
 
 
 if __name__ == "__main__":
