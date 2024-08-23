f95ec86dd047144071d973b1d2cc88bc10bfd450
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2024-08-23 15:16:36 +0000

    Add prediction with OpenAI GPT models

--- juddges/preprocessing/context_truncator.py
+++ juddges/preprocessing/context_truncator.py
@@ -1,12 +1,23 @@
 import warnings
+from abc import ABC, abstractmethod
 
+import tiktoken
 from tokenizers.implementations import BaseTokenizer
 
 
-class ContextTruncator:
-    def __init__(self, tokenizer: BaseTokenizer, max_length: int):
-        self.tokenizer = tokenizer
+class ContextTruncatorBase(ABC):
+    def __init__(self, max_length: int):
         self.max_length = max_length
+
+    @abstractmethod
+    def __call__(self, prompt: str, context: str, output: str | None = None) -> str:
+        """Truncates and returns the inner context of the input."""
+
+
+class ContextTruncator(ContextTruncatorBase):
+    def __init__(self, tokenizer: BaseTokenizer, max_length: int):
+        super().__init__(max_length)
+        self.tokenizer = tokenizer
 
         empty_messages = [
             {"role": "user", "content": ""},
@@ -41,3 +52,28 @@ class ContextTruncator:
             context, max_length=context_length, truncation=True, add_special_tokens=False
         )["input_ids"]
         return self.tokenizer.decode(context_ids)
+
+
+class ContextTruncatorTiktoken(ContextTruncatorBase):
+    """Simplified context truncation for OpenAI models."""
+
+    def __init__(self, model: str, max_length: int):
+        super().__init__(max_length)
+
+        warnings.warn("Truncator for OpenAI doesn't account for special tokens!")
+        self.model = model
+        self.tokenizer = tiktoken.encoding_for_model(model)
+
+    def __call__(self, prompt: str, context: str, output: str | None = None) -> str:
+        prompt_length = len(self.tokenizer.encode(prompt))
+
+        if output is not None:
+            output_length = len(self.tokenizer.encode(output))
+        else:
+            output_length = 0
+
+        context_length = self.max_length - prompt_length - output_length
+        if context_length <= 0:
+            raise ValueError("Prompt and output too long to keep the context!")
+
+        return self.tokenizer.decode(self.tokenizer.encode(context)[:context_length])
