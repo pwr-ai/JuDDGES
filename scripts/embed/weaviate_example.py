1176dcc48e8ae281d656b919ab8cc6a541843998
Jakub Binkowski <jakub.binkowski@pwr.edu.pl>
2024-12-01 13:01:17 +0100

    Add weaviate vectors (#31)
    * Add pre-commit and integrate it with CI
    
    * Apply pre-commit style fixes
    
    * Add sorting imports to ruff
    
    * Fix encoding in instruction dataset
    
    * Refactor and make deterministic predictions
    
    * Add llm-as-judge baseline results
    
    * Make llm-as-judge using API instead of local LLM
    
    * Fix missing attention_mask in prediction and add sampled predictions
    
    * Update data description with output_size quantile
    
    * Update packages
    
    * Refactor fine-tuning scripts and change hparams, make common code for unsloth and transformers
    
    * Refactor evaluation code
    
    * Fix unsloth peft
    
    * Add optimized unsloth inference and inference telemetry
    
    * Reproduce SFT on update and fixed code
    
    * Fix function evaluating info-extraction metrics
    
    * Evaluate updated Mistral and summarize metrics with mean and std
    
    * Fix computing maximum sequence length (account for output tokens during model's generation)
    
    * Add llm-as-judge preliminary results (too much non-evaluable)
    
    * Add unit test for parsing prediction output
    
    * Fix bug with CHRF computation
    
    * Add unit tests for chrf and fix metric params
    
    * Rollback to legacy fine-tuning config due to unsloth compatibility issue
    
    * Add mistral-nemo LLM
    
    * Add cuda version argument in Makefile
    
    * Fix llm-as-judge implementation
    
    * Disable CI on windows for now (utf-8 bugs)
    
    * Reproduce Mistral-Nemo and summarize all results
    
    * Add prediction with OpenAI GPT models
    
    * Update README.md with reproduction instruction
    
    * Migrate OpenAI interface to langchain to enable request caching
    
    * Add results for gpt-4o and gpt-4o-mini
    
    * Add Bielik v0.1 LLM
    
    Add Bielik v0.1 LLM
    
    * Add building english instruct dataset and infer over it with gpt-4o-mini
    
    * Add optional chat templating
    
    * Add new polish LLMs and separate configs for en and pl fine-tuned models
    
    * Fix pl-court-raw dataset by filtering records with empty content from
    
    * Add null-count info to pl-court-raw README
    
    * Reproduce Polish and English LLMs
    
    * Fix pre-commit exclude
    
    * Add bielik v2
    
    * Fix missing gpt-4o outputs and reproduce English data on gpt
    
    * Add llm-as-judge evaluation for English and Polish using gpt-4o-mini
    
    * Fix evaluation parser to account for non-yaml text at the beginning of the LLM output
    
    * Reproduce evaluation on fixed parsing
    
    * Fix langchain versions
    
    * Reproduce llm-as-judge with fixed prompt
    
    * Fix default num_proc in structured evaluator
    
    * Reproduce Bielik v2
    
    * Add text to chunk embeddings data and weaviate basic ingestion
    
    * Add weaviate deployment
    
    * Add two modes for weaviate ingest
    
    * Add hf-transformers vectorizer to weaviate collection
    
    * Add weaviate example
    
    * Add hf-transformer module to weaviate deployment
    
    * Add weaviate deployment info
    
    * Add usage info to weaviate README.md
    
    * Fix env variables naming
    
    * Fix env variables naming
    Fix batch length computation
    
    * Fix _moddix

---
+++ scripts/embed/weaviate_example.py
@@ -0,0 +1,38 @@
+import os
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+import weaviate
+from weaviate.collections.classes.grpc import MetadataQuery
+
+load_dotenv()
+WV_HOST = os.getenv("WV_URL", "localhost")
+WV_PORT = int(os.getenv("WV_PORT", 8080))
+WV_GRPC_PORT = int(os.getenv("WV_GRPC_PORT", 50051))
+WV_API_KEY = os.getenv("WV_API_KEY", None)
+
+QUERY_PROMPT = "zapytanie: {query}"
+
+# NOTE: This is standalone example, for convenience you can use judgements/data/weaviate_db.py
+with weaviate.connect_to_local(
+    host=WV_HOST,
+    port=WV_PORT,
+    grpc_port=WV_GRPC_PORT,
+    auth_credentials=weaviate.auth.Auth.api_key(WV_API_KEY),
+) as client:
+    coll = client.collections.get("judgement_chunks")
+    response = coll.query.hybrid(
+        query=QUERY_PROMPT.format(query="oskarżony handlował narkotykami"),
+        limit=2,
+        return_metadata=MetadataQuery(distance=True),
+    )
+
+for o in response.objects:
+    print(
+        f"{o.properties['judgment_id']} - {o.properties['chunk_id']}".center(
+            100,
+            "=",
+        )
+    )
+    pprint(o.properties["chunk_text"])
