f433a3686b5daea59ea230f7fcd2b8609b3fb847
Jakub Binkowski <jakub.binkowski@pwr.edu.pl>
2025-04-07 15:45:18 +0200

    Update pl court dataset (#35)
    * Update libraries
    
    * Improve fine tuning script and config
    
    * Add sprawy_frankowe fine-tuning
    
    * Fix buggy deepspeed version
    
    * Add SFT with deepspeed zero 2 and 3
    
    * Add script for instruction building dataset
    
    * Refine inference code and add alternative inference with vllm
    
    * Add scripts for running jobs on SLURM
    
    * Refine cluster running script, add placeholder for job logs
    
    * Fix predict_vllm
    
    * Update dvc lock with dataset
    
    * Refine sif building
    
    * FIx slurm scripts
    
    * Add testing scripts for containers and fix version to match nvidia 545 driver
    
    * Add pytest config
    
    * Remove tests for stale models
    
    * Lock version of flash attention
    
    * Refine pl-court download to allow filtering by last-update date
    
    * Improve Polish court data download and error handling
    
    * Enhance database update handling and logging in BatchDatabaseUpdate
    
    * Fix batched cursor iteration in Polish data download script
    
    * Add comprehensive Polish court mapping dataset and improve data acquisition scripts
    
    * Update raw Polish dataset
    
    * Fix gitleaks pre-commit config
    
    * Remove index from court_id mapping
    
    * Update polars
    
    * Update raw dataset
    
    * Fix nbdev_export

--- scripts/dataset/download_pl_metadata.py
+++ scripts/dataset/download_pl_metadata.py
@@ -1,10 +1,10 @@
-from typing import Optional
+from typing import Any, Optional
 
 import typer
 from dotenv import load_dotenv
 from loguru import logger
 from mpire.pool import WorkerPool
-from pymongo.errors import BulkWriteError
+from pymongo import ReplaceOne
 from requests import HTTPError
 from tenacity import (
     retry,
@@ -27,24 +27,55 @@ def main(
     batch_size: int = typer.Option(BATCH_SIZE),
     n_jobs: int = typer.Option(N_JOBS),
     limit: Optional[int] = typer.Option(None),
+    date_from: Optional[str] = typer.Option(None, help="Format: YYYY-MM-DD"),
+    date_to: Optional[str] = typer.Option(None, help="Format: YYYY-MM-DD"),
+    last_update_from: Optional[str] = typer.Option(None, help="Format: YYYY-MM-DD"),
+    last_update_to: Optional[str] = typer.Option(None, help="Format: YYYY-MM-DD"),
+    dry: bool = typer.Option(False),
 ) -> None:
+    params = {}
+    if date_from is not None:
+        params["dateFrom"] = date_from
+    if date_to is not None:
+        params["dateTo"] = date_to
+    if last_update_from is not None:
+        params["lastUpdateFrom"] = last_update_from
+    if last_update_to is not None:
+        params["lastUpdateTo"] = last_update_to
+
     api = PolishCourtAPI()
-    total_judgments = api.get_number_of_judgments()
-    logger.info(f"Total judgments found: {total_judgments}")
+    total_judgments = api.get_number_of_judgements(params=params)
+    logger.info(f"Total judgements found: {total_judgments:_}")
 
     if limit is not None:
         total_judgments = min(total_judgments, limit)
 
+    logger.info(
+        f"Downloading {total_judgments:_} judgements: (batch_size={batch_size}, n_jobs={n_jobs}, params={params})"
+    )
+    if dry:
+        return
+
     offsets = list(range(0, total_judgments, batch_size))
-    download_metadata = MetadataDownloader(mongo_uri, batch_size)
+    download_metadata = MetadataDownloader(
+        mongo_uri=mongo_uri,
+        batch_size=batch_size,
+        params=params,
+    )
     with WorkerPool(n_jobs=n_jobs) as pool:
         pool.map_unordered(download_metadata, offsets, progress_bar=True)
 
 
 class MetadataDownloader:
-    def __init__(self, mongo_uri: str, batch_size: int):
+    def __init__(
+        self,
+        mongo_uri: str,
+        batch_size: int,
+        params: dict[str, Any],
+    ):
         self.mongo_uri = mongo_uri
         self.batch_size = batch_size
+        self.params = params
 
     @retry(
         wait=wait_random_exponential(multiplier=1, min=4, max=30),
@@ -59,23 +90,24 @@ class MetadataDownloader:
             "limit": self.batch_size,
             "offset": offset,
         }
+
+        assert not any(k in params.keys() for k in self.params.keys())
+        params |= self.params
+
         api = PolishCourtAPI()
         judgments = api.get_judgments(params)
 
         for item in judgments:
             item["_id"] = item.pop("id")
 
-        # ignore when come across duplicated documents
-        try:
-            collection.insert_many(judgments, ordered=False)
-        except BulkWriteError as err:
-            if any(x["code"] != 11000 for x in err.details["writeErrors"]):
-                raise
-            else:
-                logger.warning(
-                    "found {num_duplicated} judgments duplicated",
-                    num_duplicated=len(err.details["writeErrors"]),
-                )
+        # update existing documents or insert new ones
+        write_ops = [ReplaceOne({"_id": item["_id"]}, item, upsert=True) for item in judgments]
+        result = collection.bulk_write(write_ops, ordered=False)
+        logger.info(
+            "Write results: {upserted} upserted, {modified} modified",
+            upserted=result.upserted_count,
+            modified=result.modified_count,
+        )
 
 
 if __name__ == "__main__":
