28d1098c9b85b08cf9c8eff806d357e59f7d420b
jakub.binkowski <jakub.binkowski@pwr.edu.pl>
2025-05-13 13:32:54 +0000

    Refine slurm scripts

--- scripts/utils/run_dvc_repro_on_slurm.py
+++ scripts/utils/run_dvc_repro_on_slurm.py
@@ -5,6 +5,7 @@ import sys
 import time
 from pprint import pformat
 
+import tabulate
 import typer
 from loguru import logger
 from tqdm import tqdm
@@ -15,28 +16,46 @@ MAX_WAIT_TIME = 20
 
 
 def main(
-    stage: str = typer.Argument(..., help="Stage to run"),
+    stages: list[str] = typer.Argument(..., help="Stages to run"),
     num_gpus: int = typer.Option(..., "--num-gpus", help="Number of GPUs to use"),
     account: str = typer.Option(..., "--account", help="HPC account to use"),
     partition: str = typer.Option(..., "--partition", help="Partition to use"),
     glob: bool = typer.Option(False, "--glob", help="Use glob pattern to match stages"),
+    dry: bool = typer.Option(False, "--dry", help="Dry run"),
 ):
-    stages_to_run = get_stages_to_run(stage, glob)
+    stages_to_run = get_stages_to_run(stages, glob)
 
     logger.info(f"Found {len(stages_to_run)} stages to run:\n{pformat(stages_to_run)}")
     if not typer.confirm("Do you want to run these stages?"):
         logger.info("Exiting...")
         typer.Abort()
 
+    submission_info = []
     for stage in tqdm(stages_to_run, desc="Submitting"):
-        jitter = random.uniform(MIN_WAIT_TIME, MAX_WAIT_TIME)
-        time.sleep(jitter)
-        submit_stage_to_slurm(stage=stage, num_gpus=num_gpus, account=account, partition=partition)
+        if not dry:
+            jitter = random.uniform(MIN_WAIT_TIME, MAX_WAIT_TIME)
+            time.sleep(jitter)
+        job_id, info = submit_stage_to_slurm(
+            stage=stage,
+            num_gpus=num_gpus,
+            account=account,
+            partition=partition,
+            dry=dry,
+        )
+        submission_info.append([stage, job_id, info])
+
+    print(
+        tabulate.tabulate(
+            submission_info,
+            headers=["Stage", "Job ID", "Info"],
+            tablefmt="grid",
+        )
+    )
 
 
-def get_stages_to_run(stage: str, glob: bool) -> list[str]:
+def get_stages_to_run(stages: list[str], glob: bool) -> list[str]:
     """Run dvc repro --dry and extract stages that would run."""
-    cmd = ["dvc", "repro", "-s", "--dry", "--ignore-errors"] + [stage]
+    cmd = ["dvc", "repro", "-s", "--dry", "--ignore-errors"] + stages
 
     if glob:
         cmd.append("--glob")
@@ -65,7 +84,13 @@ def get_stages_to_run(stage: str, glob: bool) -> list[str]:
         sys.exit(1)
 
 
-def submit_stage_to_slurm(stage: str, num_gpus: int, account: str, partition: str) -> str:
+def submit_stage_to_slurm(
+    stage: str,
+    num_gpus: int,
+    account: str,
+    partition: str,
+    dry: bool,
+) -> tuple[str | None, str]:
     command = [
         "sbatch",
         "--account",
@@ -83,20 +108,24 @@ def submit_stage_to_slurm(stage: str, num_gpus: int, account: str, partition: st
         f"dvc repro -s {stage}",
     ]
 
-    logger.info(f"Submitting job for stage {stage} with command: {pformat(command)}")
-    result = subprocess.run(
-        command,
-        capture_output=True,
-        text=True,
-    )
+    logger.info(f"Submitting job for stage {stage} with command: {' '.join(command)}")
 
-    if result.returncode != 0:
-        logger.error(f"Failed to submit job for stage {stage}: {result.stderr}")
-        return None
+    if not dry:
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+        )
 
-    job_id = result.stdout.strip().split()[-1]
-    logger.info(f"Submitted job {job_id} for stage {stage}")
-    return job_id
+        if result.returncode != 0:
+            logger.error(f"Failed to submit job for stage {stage}: {result.stderr}")
+            return None, result.stderr
+
+        job_id = result.stdout.strip().split()[-1]
+        logger.info(f"Submitted job {job_id} for stage {stage}")
+        return job_id, ""
+    else:
+        return None, "Dry run"
 
 
 if __name__ == "__main__":
